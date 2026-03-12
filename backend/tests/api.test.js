c7621552ccb498593212a3c936d549ee4bde15b0
Abane Zreik <abane.zreik@gmail.com>
2026-03-12 09:45:20 +0100

    Ajout des tests pour la gestion des tâches API, y compris la création, la récupération et la suppression de tâches.

--- backend/tests/api.test.js
+++ backend/tests/api.test.js
@@ -17,3 +17,65 @@ describe('API Health Check', () => {
     expect(response.body.error).toBe('Route non trouvée');
   });
 });
+
+describe('API Tasks', () => {
+  // Variable pour stocker le token JWT
+  let token = '';
+  // Pour stocker l'ID de la tâche que l'on va créer
+  let createdTaskId = ''; 
+
+  beforeAll(async () => {
+    const response = await request(app)
+      .post('/api/auth/login')
+      .send({
+        email: 'admin@test.com',
+        password: 'password'
+      });
+    
+    token = response.body.token;
+  });
+
+  it('Should refuse the access if not anny token is provided return 401 status code', async () => {
+    const response = await request(app).get('/api/tasks');
+    
+    expect(response.status).toBe(401);
+    expect(response.body.error).toBe("Token d'accès requis");
+  });
+
+  it('Should return the list ok task with a valid token return 200 status code', async () => {
+    const response = await request(app)
+      .get('/api/tasks')
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(200);
+    expect(Array.isArray(response.body)).toBeTruthy();
+  });
+
+  it('Shoudl create a new task return 201 status code', async () => {
+    const newTask = {
+      title: 'New test task',
+      description: 'test description',
+      priority: 'high'
+    };
+
+    const response = await request(app)
+      .post('/api/tasks')
+      .set('Authorization', `Bearer ${token}`)
+      .send(newTask);
+
+    expect(response.status).toBe(201);
+    expect(response.body.title).toBe(newTask.title);
+    expect(response.body).toHaveProperty('id');
+    
+    // We keep the if of the created task for the delete test
+    createdTaskId = response.body.id; 
+  });
+
+  it('Should delete the task return 204 status code', async () => {
+    const response = await request(app)
+      .delete(`/api/tasks/${createdTaskId}`)
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(204);
+  });
+});
